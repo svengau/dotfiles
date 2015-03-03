1475d5048457c8b45303d9b57dfaa368e71b9830
Sven Gaubert <sven.gaubert@playadz.com>
2015-03-03 14:09:50 +0100

    updated all files

--- init.sh
+++ init.sh
@@ -22,10 +22,4 @@
 # MISCG
 ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /bin/subl
 alias gt="kill $(ps aux | grep 'gitlab-tunnel' | awk '{print $2}');ssh gitlab-tunnel -N &"
-
-alias gitpush3="echo \"$*\" && git config --get remote.origin.url "
-
-alias gitpush='git config --get remote.origin.url && echo "$*" && git add -A && git commit -a -m "$*" && git push'
-
-
-alias='git config --get remote.origin.url && git add -A && git commit -a -m "$*" && git push'
+alias gitpush='function _gitpush(){ git config --get remote.origin.url; git add -A; git commit -a -m "$*"; git push; git log --pretty=format:"%C(yellow)%ae  %C(white)%ar %C(yellow)%s" --date=short --since="24 hours ago"}; _gitpush'
