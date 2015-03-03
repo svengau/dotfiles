368b80304bb6e82384e115bd4b388d13d0a928dc
Sven Gaubert <sven.gaubert@playadz.com>
2015-03-03 14:14:19 +0100

    moved aliases to zsh_aliases

--- init.sh
+++ init.sh
@@ -19,7 +19,7 @@
 . ./npm/init.sh
 
 
-# MISCG
+# MISC
 ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /bin/subl
 alias gt="kill $(ps aux | grep 'gitlab-tunnel' | awk '{print $2}');ssh gitlab-tunnel -N &"
 alias gitpush='function _gitpush(){ git config --get remote.origin.url; git add -A; git commit -a -m "$*"; git push; git log --pretty=format:"%C(yellow)%ae  %C(white)%ar %C(yellow)%s" --date=short --since="24 hours ago"}; _gitpush'
